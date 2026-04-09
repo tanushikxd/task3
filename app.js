821f4728ea9b4bad0cfc45e84c8663d46cd218c5
tanushikxd <tanushikxdt2012@gmail.com>
2026-04-09 11:17:11 +0100

    fixed node

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.get("/tanushikxdt2012_gmail_com", (req, res) => {
-    const x = req.query.x;
-    const y = req.query.y;
+    const { x, y } = req.query;
 
     if (
         x === undefined || y === undefined ||
         !/^\d+$/.test(x) || !/^\d+$/.test(y)
     ) {
-        return res.type("text/plain").send("NaN");
+        res.set("Content-Type", "text/plain");
+        return res.end("NaN");
     }
 
     const a = BigInt(x);
@@ -14,9 +18,7 @@ app.get("/tanushikxdt2012_gmail_com", (req, res) => {
 
     function gcd(a, b) {
         while (b !== 0n) {
-            let temp = b;
-            b = a % b;
-            a = temp;
+            [a, b] = [b, a % b];
         }
         return a;
     }
@@ -28,6 +30,10 @@ app.get("/tanushikxdt2012_gmail_com", (req, res) => {
 
     const result = lcm(a, b);
 
-    res.type("text/plain");
-    res.send(result.toString());
+    res.set("Content-Type", "text/plain");
+    res.end(result.toString());
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
 });
